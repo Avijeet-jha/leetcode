d8555e7acf2db68792cb634fcd69085e9c22850b
Avijeet Jha <avijeetjha1@gmail.com>
2026-08-24 12:47:30 +0530

    Time: 36 ms (66.81%) | Memory: 54.5 MB (64.77%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool ispalindrom(string s){
+        string s2=s;
+        reverse(s2.begin(), s2.end());
+        return s==s2;
+    }
+
+    void getAllParts(string s, vector<string> &partitions, vector<vector<string>>&ans){
+        if(s.size()==0){
+            ans.push_back(partitions);
+            return ;
+            }
+
+        for(int i=0; i<s.size(); i++){
+            string parts=s.substr(0, i+1);
+
+            if(ispalindrom(parts)){
+                partitions.push_back(parts);
+                getAllParts(s.substr(i+1), partitions, ans );
+                partitions.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>partitions;
+
+        getAllParts(s, partitions,ans);
+        return ans;
+    }
+};
