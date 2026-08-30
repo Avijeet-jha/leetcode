dc09ef485866551c9e06c48821e903b906b3edd0
Avijeet Jha <avijeetjha1@gmail.com>
2026-08-30 21:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (88.09%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -14,12 +14,12 @@ public:
         if(head1==NULL || head2==NULL){
             return head1==NULL ? head2: head1;
         }
-        //case 1
-        if(head1->val <= head2->val ){
-            head1->next= mergeTwoLists(head1->next, head2);
+
+        if(head1->val <= head2->val){
+            head1->next = mergeTwoLists(head1->next , head2);
             return head1;
         }else{
-            head2->next=mergeTwoLists(head1,head2->next);
+            head2->next=mergeTwoLists(head1, head2->next);
             return head2;
         }
     }
